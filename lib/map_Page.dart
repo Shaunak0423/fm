c3391a8f668569c9b5cb0032e75a67c5098a433f
Shaunak Shet <111705360+Shaunak0423@users.noreply.github.com>
2024-06-03 19:58:21 +0530

    initial commit

---
+++ lib/map_Page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class mapPage extends StatefulWidget {
+  const mapPage({super.key});
+
+  @override
+  State<mapPage> createState() => _mapPageState();
+  
+}
+
+class _mapPageState extends State<mapPage> {
+  static const LatLng _position = LatLng(15.49770, 73.81929);
+  static const LatLng _posi = LatLng(15.52632, 73.82701);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _position,
+          zoom: 12,
+        ),
+        markers: {
+          Marker(
+            markerId: MarkerId("_currentLocation"),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _position,
+          )
+        },
+      ),
+    );
+  }
+}
